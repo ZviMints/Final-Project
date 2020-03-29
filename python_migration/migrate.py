86b333c2c12489e0971e4d1198453cd033f83a66
ZviMints <zvimints@gmail.com>
2020-03-29 22:26:45 +0300

    fix python_migration

--- python_migration/migrate.py
+++ python_migration/migrate.py
@@ -1,73 +1,91 @@
 from grakn.client import GraknClient
 import ijson
 
-def build_phone_call_graph(input): # Step 2
+class conversationWrapper:
+    def __init__(self, id, messages):
+        self.id = id
+        self.messages = messages
+
+def build_phone_call_graph(input):
     with GraknClient(uri="localhost:48555") as client:
-        with client.session(keyspace = "graph") as session: # graph is the name of the keyspace
+        with client.session(keyspace = "phone_calls") as session: # graph is the name of the keyspace
                 load_data_into_grakn(input, session)
 
-def load_data_into_grakn(input, session): # Step 3
+def load_data_into_grakn(input, session):
+    print("connected successfully...")
     conversations = parse_data_to_dictionaries(input) # gets all conversations
-
+    print("got all conversations, starting to process...")
+    
     for conversation in conversations: # item is now one conversation
         with session.transaction().write() as transaction:
-            graql_insert_query = input["template"](conversation)
-            print("Executing Graql Query: " + graql_insert_query)
-            transaction.query(graql_insert_query)
+            (firstQuery,first_user_id) = insertFirstUser(conversation)
+            (secondQuery,second_user_id) = insertSecondUser(conversation)
+            (thirdQuery) = createConnectedRelation(conversation,first_user_id,second_user_id)
+
+            print("Executing Graql Query: " + firstQuery)
+            transaction.query(firstQuery)
+
+            print("Executing Graql Query: " + secondQuery)
+            transaction.query(secondQuery)
+
+            print("Executing Graql Query: " + thirdQuery)
+            transaction.query(thirdQuery)
+
             transaction.commit()
 
     print("\nInserted " + str(len(conversations)) + " items from [ " + input["data_path"] + "] into Grakn.\n")
 
-def conversation_template(conversation):
-    # insert conversation
+# Insert first Author at conversation
+def insertFirstUser(conversation):
+    first_user_id = conversation.messages[0]["author"]
+    return ('insert $user isa user, has user_id "' + first_user_id + '";', first_user_id)
 
-    # Safety check if message list is empty then return
-    first_user_id = "" # First author
-    graql_insert_query = 'insert $user isa person, has user_id "' + first_user_id
-
+# Insert second Author at conversation
+def insertSecondUser(conversation):
     # loop to find the second user
-    second_user_id = "" # Second author
-    graql_insert_query = 'insert $user isa person, has user_id "' + second_user_id
+    first_user_id = conversation.messages[0]["author"]
+    second_user_id = first_user_id
+    index = 0
+    while second_user_id == first_user_id:
+        second_user_id = conversation.messages[index]["author"]
+        index = index + 1
 
+        return ('insert $user isa user, has user_id "' + second_user_id + '";', second_user_id)
 
-    # Need to initialize relation between the two users with "play talk"
-
-    # Need to initialize connected relation with "@id"
-
-    # Match company | first user
-    graql_insert_query = 'match #user isa company, has user_id "' + first_user_id + ';'
-    # Match person | second user
-    graql_insert_query += 'match #user isa company, has user_id "' + second_user_id + ';'
-    # insert contract | connected relation
-    graql_insert_query += " insert (talk: $user, talk: $user) isa connected, has conversation_id " + conversation["@id"] + ';'
-
-
-
-    # ------------------------- Correct
-    graql_insert_query = 'insert $person isa person, has phone-number "' + person["phone_number"] + '"'
-    if "first_name" in person:
-        # person is a customer
-        graql_insert_query += ", has is-customer true"
-        graql_insert_query += ', has first-name "' + person["first_name"] + '"'
-        graql_insert_query += ', has last-name "' + person["last_name"] + '"'
-        graql_insert_query += ', has city "' + person["city"] + '"'
-        graql_insert_query += ", has age " + str(person["age"])
-    else:
-        # person is not a customer
-        graql_insert_query += ", has is-customer false"
-    graql_insert_query += ";"
+# Create connection between first author and second author
+def createConnectedRelation(conversation,first_user_id,second_user_id):
+    # Match first user
+    graql_insert_query = 'match $user1 isa user, has user_id "' + first_user_id + '";'
+    # Match  second user
+    graql_insert_query += '$user2 isa user, has user_id "' + second_user_id + '";'
+    # insert connected relation
+    graql_insert_query += 'insert $new-connection(talk: $user1, talk: $user2) isa connected; $new-connection has conversation_id "' + conversation.id + '"; ' # [V]
     return graql_insert_query
 
-def parse_data_to_dictionaries(input): # Step 4
-    items = []
+def parse_data_to_dictionaries(input):
+    conversations = []
     with open(input["data_path"] + ".json") as data:
-        for item in ijson.items(data, "item"):
-            items.append(item)
-    return items
+        print("successfully opened " + input["data_path"] + ".json")
+        for root in ijson.items(data, 'conversations.conversation'): #
+            for conversation in root:
+                id = conversation["@id"]
+                messages = []
+                for msg in conversation["message"]:
+                    messages.append(msg)
+                conversations.append( conversationWrapper(id,messages) )
+                print("added conversation number: " + id)
+    return conversations
 
 input = {
-        "data_path": "files/phone-calls/data/people",
-        "template": conversation_template
+        "data_path": "/Users/Zvi/Desktop/FinalProject/Dataset/pan12-sexual-predator-identification-training-corpus-2012-05-01/small_train",
 }
 
-build_phone_call_graph(input) # Step 1
+# build_phone_call_graph(input)
+build_phone_call_graph(input)
+
+
+
+
+
+
+
