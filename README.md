ffa3b8bb6399fb9dde46ac280dba2b375682ff45
ZviMints <zvimints@gmail.com>
2020-04-05 14:33:10 +0300

    fix python migration

--- README.md
+++ README.md
@@ -1,44 +1,9 @@
-[![Ariel Logo](https://camo.githubusercontent.com/c6cb93c822ddc0d3d2504cacb8f4f5b591fcba38/68747470733a2f2f6d656469612e6c6963646e2e636f6d2f646d732f696d6167652f433444304241514744376e704d506f58796d772f636f6d70616e792d6c6f676f5f3230305f3230302f303f653d3231353930323434303026763d6265746126743d545a3855623355326a435a6741315f6b4178365345386a6d4163534a6b62695a41754e364b6e315f6c5730 "Ariel Logo")](https://camo.githubusercontent.com/c6cb93c822ddc0d3d2504cacb8f4f5b591fcba38/68747470733a2f2f6d656469612e6c6963646e2e636f6d2f646d732f696d6167652f433444304241514744376e704d506f58796d772f636f6d70616e792d6c6f676f5f3230305f3230302f303f653d3231353930323434303026763d6265746126743d545a3855623355326a435a6741315f6b4178365345386a6d4163534a6b62695a41754e364b6e315f6c5730 "Ariel Logo")
-#### Final Project - `Pedophilles Community Detection in Knowledge Graphs`
-- `Eilon Tsadok`
-- `Zvi Mints`<br>
-Managed by `Or Anidjar` and `Amit Dvir`
-# Motivation
-In this project, we will explore how to benefit the knowledge that can be extracted
-from a `Knowledge Graph`, in order to detect a community of pedophilles with some
-charachteristics.
-As such, we will represent in this Knowledge Graph an IM dataset containing users
-that served as pedophilles impersonators and their textual content shared over the
-Internet. For the task of*"understanding"*the content, we will take advantage of the
-recently based language modelling developed by Google corpora, which is called
-`BERT` **(Bidirectional Encoder Representations from Transformers).**
-After the knowledge representation of the data set in the embedded knowledge
-graph, we will apply a spectral clustering algorithm in order to detect communities
-of users that are sharing close enough behavioral patterns.
-# Useful Links
-- **Dataset Source**
-	- https://pan.webis.de/clef12/pan12-web/sexual-predator-identification.html
-	- `.json` format **training** dataset: https://f2h.io/8b3xzuy6rnq4
-	- `.json` format **test** dataset: https://f2h.io/5n7mcc7cx3b6 	
-- **Knowledge Graph Vertices Embedding**
-	- https://github.com/jimmywangheng/knowledge_representation_pytorch
-	- https://radimrehurek.com/gensim/models/word2vec.html
-- **PCA (Principal Component Analysis)**
-	- https://en.wikipedia.org/wiki/Principal_component_analysis
-	- https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
-- **Spectral Clustering**
-	- https://en.wikipedia.org/wiki/Spectral_clustering
-	- https://scikitlearn.org/stable/modules/generated/sklearn.cluster.SpectralClustering.html
-	- https://en.wikipedia.org/wiki/Elbow_method_(clustering)
-- **Topic Extraction**
-	- https://en.wikipedia.org/wiki/Topic_model
-	- https://towardsdatascience.com/the-complete-guide-for-topicsextraction-in-python-a6aaa6cedbbc
-- **Google BERT Model Utilization in Python**
-	+ https://towardsdatascience.com/word-embedding-using-bert-inpython-dd5a86c003423
+done
+    val u = Json.obj("$set" -> Json.obj(ReplayDeadLetter.Status -> Status.Done))
 
-# Scheduling
-- **Start Date:** 24/03/2020
-- **End Date:** `Start Date` + 12 week
+    with error
+        val u = Json.obj(fields = "$push" -> Json.obj(ReplayDeadLetter.Errors -> exception.getMessage))
 
-# Winning Plan
-[![Plain](https://i.ibb.co/DzG2QJG/Screen-Shot-2020-03-24-at-15-05-50.png "Plain")](https://i.ibb.co/DzG2QJG/Screen-Shot-2020-03-24-at-15-05-50.png "Plain")
+failed
+      "$set" -> Json.obj(ReplayDeadLetter.Status -> Status.Failed),
+      "$push" -> Json.obj(ReplayDeadLetter.Errors -> exception.getMessage))
--- python_migration/migrate.py
+++ python_migration/migrate.py
@@ -1,26 +1,31 @@
 from grakn.client import GraknClient
 import ijson
 
+
 class conversationWrapper:
     def __init__(self, id, messages):
         self.id = id
         self.messages = messages
 
-def build_phone_call_graph(input):
+
+def build(input):
     with GraknClient(uri="localhost:48555") as client:
-        with client.session(keyspace = "phone_calls") as session: # graph is the name of the keyspace
-                load_data_into_grakn(input, session)
+        with client.session(keyspace="conversations_graph") as session:  # graph is the name of the keyspace
+            load_data_into_grakn(input, session)
+
 
 def load_data_into_grakn(input, session):
     print("connected successfully...")
-    conversations = parse_data_to_dictionaries(input) # gets all conversations
+    conversations = parse_data_to_dictionaries(input)  # gets all conversations
     print("got all conversations, starting to process...")
-    
-    for conversation in conversations: # item is now one conversation
+    i = 0
+    for conversation in conversations:  # item is now one conversation
         with session.transaction().write() as transaction:
-            (firstQuery,first_user_id) = insertFirstUser(conversation)
-            (secondQuery,second_user_id) = insertSecondUser(conversation)
-            (thirdQuery) = createConnectedRelation(conversation,first_user_id,second_user_id)
+            print("We are looking at conversation number: " + i )
+            i = i + 1
+            (firstQuery, first_user_id) = insertFirstUser(conversation)
+            (secondQuery, second_user_id) = insertSecondUser(conversation)
+            (thirdQuery) = createConnectedRelation(conversation, first_user_id, second_user_id)
 
             print("Executing Graql Query: " + firstQuery)
             transaction.query(firstQuery)
@@ -35,10 +40,12 @@ def load_data_into_grakn(input, session):
 
     print("\nInserted " + str(len(conversations)) + " items from [ " + input["data_path"] + "] into Grakn.\n")
 
+
 # Insert first Author at conversation
 def insertFirstUser(conversation):
     first_user_id = conversation.messages[0]["author"]
     return ('insert $user isa user, has user_id "' + first_user_id + '";', first_user_id)
+
 
 # Insert second Author at conversation
 def insertSecondUser(conversation):
@@ -52,40 +59,37 @@ def insertSecondUser(conversation):
 
         return ('insert $user isa user, has user_id "' + second_user_id + '";', second_user_id)
 
+
 # Create connection between first author and second author
-def createConnectedRelation(conversation,first_user_id,second_user_id):
+def createConnectedRelation(conversation, first_user_id, second_user_id):
     # Match first user
     graql_insert_query = 'match $user1 isa user, has user_id "' + first_user_id + '";'
     # Match  second user
     graql_insert_query += '$user2 isa user, has user_id "' + second_user_id + '";'
     # insert connected relation
-    graql_insert_query += 'insert $new-connection(talk: $user1, talk: $user2) isa connected; $new-connection has conversation_id "' + conversation.id + '"; ' # [V]
+    graql_insert_query += 'insert $new-connection(talk: $user1, talk: $user2) isa connected; $new-connection has conversation_id "' + conversation.id + '"; '  # [V]
     return graql_insert_query
+
 
 def parse_data_to_dictionaries(input):
     conversations = []
     with open(input["data_path"] + ".json") as data:
         print("successfully opened " + input["data_path"] + ".json")
-        for root in ijson.items(data, 'conversations.conversation'): #
+        for root in ijson.items(data, 'conversations.conversation'):  #
             for conversation in root:
                 id = conversation["@id"]
                 messages = []
                 for msg in conversation["message"]:
                     messages.append(msg)
-                conversations.append( conversationWrapper(id,messages) )
+                conversations.append(conversationWrapper(id, messages))
                 print("added conversation number: " + id)
     return conversations
 
+
 input = {
-        "data_path": "/Users/Zvi/Desktop/FinalProject/Dataset/pan12-sexual-predator-identification-training-corpus-2012-05-01/small_train",
+    "data_path": "/Users/Zvi/Desktop/FinalProject/Dataset/pan12-sexual-predator-identification-training-corpus-2012-05-01/small_train",
 }
 
-# build_phone_call_graph(input)
-build_phone_call_graph(input)
-
-
-
-
-
-
+# build_conversations_graph(input)
+build(input)
 
