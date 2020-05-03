a30ae158dd288ab9e6c8171b009e22fb8cb3f2e1
Eilon Tsadok <37323667+eilon26@users.noreply.github.com>
2020-05-03 13:04:16 +0300

    spectral clastering elbow

--- Community_Detection_With_Algoritms/algorithms/SpectralClustering.py
+++ Community_Detection_With_Algoritms/algorithms/SpectralClustering.py
@@ -2,12 +2,35 @@ import matplotlib as matplotlib
 from sklearn.cluster import SpectralClustering as SpectralClusteringAlgorithm # Algorithm
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
 matplotlib.use('MacOSX')
 
 class SpectralClustering:
     def __init__(self, vectors_3dim, color):
         self.vectors_3dim = vectors_3dim
         self.color = color
+
+    def find_elbow(self):
+        mms = MinMaxScaler()
+        mms.fit(self.vectors_3dim)
+        data_transformed = mms.transform(self.vectors_3dim)
+
+        Sum_of_squared_distances = []
+        K = range(1, int(0.01 * len(data_transformed)))
+        for k in K:
+            sc = SpectralClusteringAlgorithm(n_clusters=k)
+            sc = sc.fit(self.vectors_3dim)
+            # Sum_of_squared_distances.append(sc.inertia_)
+            # if (len(Sum_of_squared_distances)) > 1:
+            #     gradient = Sum_of_squared_distances[k - 1] - Sum_of_squared_distances[k - 2]
+            #     if gradient > -1000:
+            #         return k - 1
+        plt.plot(K, Sum_of_squared_distances, 'bx-')
+        plt.xlabel('k')
+        plt.ylabel('Sum_of_squared_distances')
+        plt.title('Elbow Method For Optimal k')
+        plt.show()
 
     def getPlot(self):
         df = pd.DataFrame(
@@ -27,7 +50,7 @@ class SpectralClustering:
         )
 
         sc = SpectralClusteringAlgorithm(n_clusters=20, assign_labels="discretize",  random_state=0)
-        labels = sc.fit_predict(self.vectors_3dim)
+        sc.fit_predict(self.vectors_3dim)
         base_figure.scatter(sc.affinity_matrix_[:,0] , sc.affinity_matrix_[:, 1], sc.affinity_matrix_[:, 2], s=7000,c=self.color)
         base_figure.set_xlabel('pca-one')
         base_figure.set_ylabel('pca-two')
