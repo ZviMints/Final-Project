de96259e6824e2312cb543c3d6952908844d4cb8
ZviMints <zvimints@gmail.com>
2020-05-14 21:20:54 +0300

    added Procfile

--- API/client/server/application.py
+++ API/client/server/application.py
@@ -1,12 +1,17 @@
-
 import logging
+
+from threading import Thread
+
 from flask import Flask, render_template
 from flask import jsonify
 
-import networkx as nx
-from node2vec import Node2Vec
+from networkx.readwrite import json_graph
+
+import networkx
+import matplotlib
 import matplotlib.pyplot as plt
 
+# Serve the file over http to allow for cross origin requests
 app = Flask(__name__)
 
 # Zvi Mints And Eilon Tsadok
@@ -22,26 +27,34 @@ def load(dataset):
 
     #making G (networkx)
     if dataset == "pan12-sexual-predator-identification-training-corpus-2012-05-01":
-        G = nx.read_multiline_adjlist("./load/train_networkxBeforeRemove.adjlist")
+        G = networkx.read_multiline_adjlist("./load/train_networkxBeforeRemove.adjlist")
     elif dataset == "pan12-sexual-predator-identification-test-corpus-2012-05-17":
-        G = nx.read_multiline_adjlist("./load/test_networkxBeforeRemove.adjlist")
+        G = networkx.read_multiline_adjlist("./load/test_networkxBeforeRemove.adjlist")
     else:
            return jsonify(err="405 Method Not Allowed")
 
     # Generate picture of networkx
-#    nx.draw(G, node_size=1)
-#    matplotlib.pyplot.savefig("../public/models/load/models/load/networkx_before_remove.png")
+    # networkx.draw(G, node_size=1)
+    # plt.savefig("./load/results/networkx_before_remove.png")
     app.logger.debug('loaded dataset with %s nodes before remove' % len(G.nodes()))
 
+    # write json formatted data
+    before = json_graph.node_link_data(G)  # node-link format to serialize
+
     # After Remove
-    for component in list(nx.connected_components(G)):
+    for component in list(networkx.connected_components(G)):
         if len(component) <= 2: # This will actually remove only 2-connected
             for node in component:
                 G.remove_node(node)
 
     app.logger.debug('loaded dataset with %s nodes after remove' % len(G.nodes()))
 
-    nx.draw(G, node_size=3)
-    plt.savefig("../API/client/public/models/load/networkx_after_remove.png")
+    # write json formatted data
+    after = json_graph.node_link_data(G)  # node-link format to serialize
 
-    return jsonify(before="networkx_before_remove.png", after="networkx_after_remove.png")
+    # networkx.draw(G, node_size=3)
+    plt.savefig("./load/results/networkx_after_remove.png")
+    app.logger.debug('finished load(G)')
+
+    plt.close()
+    return jsonify(before=before, after=after)
