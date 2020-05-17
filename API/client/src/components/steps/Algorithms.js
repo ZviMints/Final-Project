d8dd13b9f5c4fe32a91b695a5c3fcd474c663325
ZviMints <zvimints@gmail.com>
2020-05-17 20:43:39 +0300

    fixed version

--- API/client/src/components/steps/Algorithms.js
+++ API/client/src/components/steps/Algorithms.js
@@ -4,11 +4,6 @@ import React, { Component } from 'react';
 export default class Algorithms extends Component {
   constructor(props) {
     super(props);
-    this.state ={
-      connected: true,
-      spectral: true,
-      kmeans: true
-    }
   }
 
   handleInputChange = (event) => {
@@ -17,22 +12,18 @@ export default class Algorithms extends Component {
     if (target.checked){
       this.props.updateAlgorithm(algorithm);
     } else this.props.removeAlgorithm(algorithm);
-
-    this.setState({
-      isChecked: !this.state.isChecked,
-    });
   }
 
   render() {
         return (
             <div className="algorithms">
-              <input type="checkbox" defaultChecked={this.state.kmeans} id="algo1" name="kmeans" onChange={this.handleInputChange}/>
+              <input type="checkbox" id="algo1" name="kmeans" onChange={this.handleInputChange}/>
               <label htmlFor="kmeans">KMeans</label> <br />
 
-              <input type="checkbox" defaultChecked={this.state.spectral} id="algo2" name="spectral" onChange={this.handleInputChange}/>
+              <input type="checkbox" id="algo2" name="spectral" onChange={this.handleInputChange}/>
               <label htmlFor="spectral">Spectral Clustering</label> <br />
 
-              <input type="checkbox" defaultChecked={this.state.connected} id="algo3" name="connected" onChange={this.handleInputChange}/>
+              <input type="checkbox" id="algo3" name="connected" onChange={this.handleInputChange}/>
               <label htmlFor="connected">Connected Components</label> <br />
             </div>
           );
